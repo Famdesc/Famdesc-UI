459e4f3c43e30747bf63bee9fb74aac59d3df5c3
Nardis Del Campo Soler <48690649+nardis9501@users.noreply.github.com>
2024-11-17 14:24:06 -0500

    Revert "New feature"

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,11 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-interface ImportMetaEnv {
-    readonly CONTACT_FORM_TOKEN: string;
-    readonly SCRIPT_API_URL: string;
-    
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv;
-  }
