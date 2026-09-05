602d5aa0a10323fbba397e5abcdee0a4e2720fab
Nardis <nardis9501@gmail.com>
2026-09-05 06:59:38 -0400

    feat(navigation): add "Servicios" link to navbar and footer

--- src/utils/es/navigation.ts
+++ src/utils/es/navigation.ts
@@ -1,6 +1,7 @@
 const navBarLinks = [
   { name: "Inicio", url: "/es" },
   { name: "Productos", url: "/products" },
+  { name: "Servicios", url: "/es/services" },
   { name: "Hiring", url: "/es/hiring" },
   { name: "Nosotros", url: "/es/about" },
   { name: "Contacto", url: "/es/contact" },
@@ -12,6 +13,7 @@ const footerLinks = [
     links: [
       { name: "Productos FSA", url: "/products" },
       { name: "FSA Line One Pro", url: "/products/fsa-line-one-pro" },
+      { name: "Famdesc Web", url: "/es/services" },
     ],
   },
   {
